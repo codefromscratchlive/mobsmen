befb78f3bc5cf2fc17a2c3479b17ef445f8cbf0a
Code From Scratch LIVE <codefromscratchlive@gmail.com>
2025-09-08 14:07:30 +0200

    Fix build file according to naming conventions

--- web/build.ts
+++ web/build.ts
@@ -5,16 +5,16 @@ async function main() {
 }
 
 async function build_client() {
-  const missionsDir = './src/missions';
-  const missionFiles = (await fs.readdir(missionsDir, { withFileTypes: true }))
+  const missions_dir = './src/missions';
+  const mission_files = (await fs.readdir(missions_dir, { withFileTypes: true }))
     .filter(dirent => dirent.isFile() && dirent.name.endsWith('.ts'))
-    .map(dirent => `${missionsDir}/${dirent.name}`);
+    .map(dirent => `${missions_dir}/${dirent.name}`);
 
   await Bun.build({
-    entrypoints: ["./src/web.ts", ...missionFiles],
+    entrypoints: ["./src/web.ts", ...mission_files],
     outdir: "./build/js/",
     define: {},
-    sourcemap: "external"
+    sourcemap: "external",
   });
 }
 
